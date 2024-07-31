1db3eeeffcaefe4fe8d23bf9135dd7093a6c2be9
Danilo Sanchez <danilosanchez.dev@gmail.com>
2024-07-31 18:46:00 -0300

    Finalizada a versão 1 Desktop

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,60 @@
-function criptografar(){
-    console.log('Botão criptografar clicado');
-};
+const textArea = document.querySelector('.principal__conteudo__texto');
+const mensagem = document.querySelector('.principal__resultado__texto');
+const mensagemInicialTitulo = document.querySelector('.principal__resultado__subtitulo');
+const mensagemInicialParagrafo = document.querySelector('.principal__resultado__paragrafo');
 
-function descriptografar(){
-    console.log('Botão descriptografar clicado');
-};
+
+function btnCriptografar(){
+    const textoEncriptado = criptografar(textArea.value);
+    mensagem.value = textoEncriptado;
+    textArea.value = "";
+    mensagem.style.setProperty('background-image', 'none');
+    mensagemInicialTitulo.style.setProperty('display', 'none');
+    mensagemInicialParagrafo.style.setProperty('display', 'none');
+}
+
+function criptografar(stringEncriptada){
+
+    let matrizCodigo = [['e', 'enter'], ['i', 'imes'], ['a', 'ai'], ['o', 'ober'], ['u', 'ufat']];
+
+    stringEncriptada = stringEncriptada.toLowerCase();
+    
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if (stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+
+    }
+
+    return stringEncriptada;
+}
+
+function btnDescriptografar(){
+    const textoDecriptado = descriptografar(textArea.value);
+    mensagem.value = textoDecriptado;
+    textArea.value = "";
+    mensagemInicialTitulo.style.setProperty('display', 'none');
+    mensagemInicialParagrafo.style.setProperty('display', 'none');
+}
+
+function descriptografar(stringDecriptada){
+
+    let matrizCodigo = [['e', 'enter'], ['i', 'imes'], ['a', 'ai'], ['o', 'ober'], ['u', 'ufat']];
+
+    stringDecriptada = stringDecriptada.toLowerCase();
+    
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if (stringDecriptada.includes(matrizCodigo[i][1])){
+            stringDecriptada = stringDecriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+
+    }
+
+    return stringDecriptada;
+}
+
+function btnCopiar(){
+    mensagemInicialParagrafo.innerHTML = 'Texto copiado para a área de transferência!';
+    mensagemInicialParagrafo.style.removeProperty('display', 'none');
+    navigator.clipboard.writeText(mensagem.value);
+}
